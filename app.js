ce82eb8d2a88390945265444da4868c4ab3d7b8d
Pierre BIZIEN <pierre.bizien@edu.escp.eu>
2023-05-10 20:00:48 +0200

    Postgres configure en local et prisma en cours

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  // console.log('appel via api stuff')
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
   next();
 });
+
 
 app.use((req, res, next) => {
   res.status(201);
